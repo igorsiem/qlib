5284f05a8d293dcee58e30e68ccbf56f43c0dc30
Igor Siemienowicz <igor@qprise.com>
2018-11-22 09:58:27 +0800

    added doxygen support

---
+++ qlib.h
@@ -0,0 +1,23 @@
+/**
+ * \file qlib.h
+ * Single-point include file for qLib
+ * 
+ * \author Igor Siemienowicz
+ * 
+ * \copyright Copyright Igor Siemienowicz 2018 Distributed under the Boost
+ * Software License, Version 1.0. (See accompanying file ../LICENSE_1_0.txt
+ * or copy at https://www.boost.org/LICENSE_1_0.txt
+ */
+
+#include "strutils.h"
+
+/**
+ * \mainpage Welcome to qLib
+ * 
+ * This is the documentation for *qLib*, a set of miscellaneous C++ utilities
+ * written by Igor Siemienowicz (igor@qprise.com).
+ * 
+ * Copyright Igor Siemienowicz 2018 Distributed under the Boost
+ * Software License, Version 1.0. (See accompanying file LICENSE_1_0.txt or
+ * copy at https://www.boost.org/LICENSE_1_0.txt
+ */
